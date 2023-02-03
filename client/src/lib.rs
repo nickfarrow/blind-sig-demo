45efb9ba26d2f404b497eafe92b23d4ff86ce2f6
Nick Farrow <nick@nickfarrow.com>
2023-02-03 11:43:45 +1100

    Mousedown area

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -8,6 +8,9 @@ use schnorr_fun::{
 use sha2::Sha256;
 use wasm_bindgen::prelude::*;
 
+use gloo::{events::EventListener};
+use wasm_bindgen::JsCast;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -87,5 +90,72 @@ pub fn gen_nonce() -> Result<(), JsValue> {
 
     body.append_child(&val)?;
 
+    Ok(())
+}
+
+pub fn eventlistener_new_p_mousedown() {
+    let window = web_sys::window().expect("global window does not exists");
+    let document = window.document().expect("expecting a document on window");
+    let body = document
+        .body()
+        .expect("document expect to have have a body");
+
+    let paragraph = document
+        .create_element("p")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlParagraphElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    paragraph.set_align("center");
+    paragraph.set_inner_html("<br />Click within this boundary to test the mousedown event. <br />Check the results in your web console.<br /><br />");
+
+    paragraph
+        .style()
+        .set_property("border", "solid")
+        .map_err(|_| ())
+        .unwrap();
+
+    let on_down = EventListener::new(&paragraph, "mousedown", move |_event| {
+        web_sys::console::log_1(&"Paragrapah mousedown".into());
+    });
+    on_down.forget();
+    body.append_child(&paragraph).unwrap();
+}
+
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    eventlistener_new_p_mousedown();
+    // let window = web_sys::window().expect("no global `window` exists");
+    // let document = window.document().expect("should have a document on window");
+    // let body = document.body().expect("document should have a body");
+
+    // let search_div = document.create_element("div")?;
+    // search_div.set_id("searchbox");
+
+    // let input_box = document.create_element("input")?;
+    // input_box.set_attribute("type", "text");
+    // input_box.set_id("name");
+    // search_div.append_child(&input_box)?;
+
+    // let submit_box = document.create_element("input")?;
+    // submit_box.set_attribute("type", "button");
+    // submit_box.set_attribute("value", "Submit");
+    // submit_box.set_attribute("name", "submit");
+    // submit_box.set_id("submit");
+
+    // search_div.append_child(&submit_box)?;
+    // body.append_child(&search_div)?;
+
+    // let on_click = EventListener::new(&submit_box, "click", move |_event| {
+    //     let value = document
+    //         .get_element_by_id("name")
+    //         .unwrap()
+    //         .node_value()
+    //         .unwrap();
+    //     alert(&value);
+    // });
+
+    // on_click.forget();
     Ok(())
 }
