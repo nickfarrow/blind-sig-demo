b511dff5f0cf6a7616d418f2b01d9c2f6f9427b8
Nick Farrow <nick@nickfarrow.com>
2023-02-03 21:27:56 +1100

    cleanup old files

--- database_scripts/down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE nonces;
-DROP TABLE messages;
--- database_scripts/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE nonces (
-    secret_nonce VARCHAR(255) NOT NULL,
-	public_nonce VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE messages (
-    msg VARCHAR(255) NOT NULL
-);
